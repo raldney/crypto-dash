678ede198b663d8db7c3297fb706f7fb1143b956
Raldney <raldney.alves@gmail.com>
2022-08-05 18:32:29 -0300

    feat: criando dashboard para mostrar os cards de cripto

---
+++ client/src/components/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="grid grid-cols-3 xl:grid-cols-4 gap-4 m-4">
+    <div class="card-block" v-for="coin in watchlist" :key="coin.symbol">
+      <CoinCard :coin="coin || {}" :info="coin"></CoinCard>
+    </div>
+  </div>
+</template>
+<script>
+import { useCryptoStore } from "../stores/crypto";
+import CoinCard from "@/components/CoinCard.vue";
+
+import { mapState } from "pinia";
+
+export default {
+  name: "DashBoard",
+  components: {
+    CoinCard,
+  },
+  computed: {
+    ...mapState(useCryptoStore, {
+      watchlist: "watchlist",
+    }),
+  },
+};
+</script>
